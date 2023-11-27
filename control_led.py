8231bd95f4a823c00d357cab3e6fd1bd8a8d86c4
Redyana Marbun <85381231+redyana18@users.noreply.github.com>
2023-11-27 17:06:30 +0700

    Update control_led.py

--- control_led.py
+++ control_led.py
@@ -7,7 +7,7 @@ GPIO.setup(
     17, GPIO.OUT, initial=GPIO.LOW
 )  # Set pin 8 to be an output pin and set initial value to low (off)
 while True:  # Run forever
-    GPIO.output(1, GPIO.HIGH)  # Turn on
-    sleep(300)  # Sleep for 1 second
-    GPIO.output(1, GPIO.LOW)  # Turn off
-    sleep(300)  # Sleep for 1 second
+    GPIO.output(17, GPIO.HIGH)  # Turn on
+    sleep(1)  # Sleep for 1 second
+    GPIO.output(17, GPIO.LOW)  # Turn off
+    sleep(1)  # Sleep for 1 second
