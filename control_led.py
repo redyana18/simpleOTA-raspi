dbbe9fd30c3f9d5eb77ec25ebf6bf35a0c6f5825
Redyana Marbun <85381231+redyana18@users.noreply.github.com>
2023-11-27 16:36:08 +0700

    Update control_led.py

--- control_led.py
+++ control_led.py
@@ -4,10 +4,10 @@ from time import sleep  # Import the sleep function from the time module
 GPIO.setwarnings(False)  # Ignore warning for now
 GPIO.setmode(GPIO.BOARD)  # Use physical pin numbering
 GPIO.setup(
-    4, GPIO.OUT, initial=GPIO.LOW
+    7, GPIO.OUT, initial=GPIO.LOW
 )  # Set pin 8 to be an output pin and set initial value to low (off)
 while True:  # Run forever
-    GPIO.output(4, GPIO.HIGH)  # Turn on
+    GPIO.output(7, GPIO.HIGH)  # Turn on
     sleep(1)  # Sleep for 1 second
-    GPIO.output(4, GPIO.LOW)  # Turn off
+    GPIO.output(7, GPIO.LOW)  # Turn off
     sleep(1)  # Sleep for 1 second
