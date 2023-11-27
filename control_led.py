75ed80eacd8cf1a01717ff5deb595f71b7090b20
Redyana Marbun <85381231+redyana18@users.noreply.github.com>
2023-11-27 16:51:27 +0700

    Update control_led.py

--- control_led.py
+++ control_led.py
@@ -2,12 +2,12 @@ import RPi.GPIO as GPIO  # Import Raspberry Pi GPIO library
 from time import sleep  # Import the sleep function from the time module
 
 GPIO.setwarnings(False)  # Ignore warning for now
-GPIO.setmode(GPIO.BOARD)  # Use physical pin numbering
+GPIO.setmode(GPIO.BCM)  # Use physical pin numbering
 GPIO.setup(
-    7, GPIO.OUT, initial=GPIO.LOW
+    17, GPIO.OUT, initial=GPIO.LOW
 )  # Set pin 8 to be an output pin and set initial value to low (off)
 while True:  # Run forever
-    GPIO.output(7, GPIO.HIGH)  # Turn on
+    GPIO.output(17, GPIO.HIGH)  # Turn on
     sleep(1)  # Sleep for 1 second
-    GPIO.output(7, GPIO.LOW)  # Turn off
+    GPIO.output(17, GPIO.LOW)  # Turn off
     sleep(1)  # Sleep for 1 second
